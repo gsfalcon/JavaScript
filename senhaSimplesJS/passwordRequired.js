675af439516e1f04ad8705a890ba8c44cf63c14d
Falcon <guiedsouza@gmail.com>
2020-04-09 15:01:27 -0300

    update list

---
+++ senhaSimplesJS/passwordRequired.js
@@ -0,0 +1,10 @@
+var senha = 1234 //default password
+var senhaInserida = window.prompt("Digite a senha para continuar: ") //password entered
+    if (senha != senhaInserida) //checking password
+    {
+        window.alert("SENHA INCORRETA / WRONG PASSWORD") //wrong password error message
+        top.location.href = "error.html" //redirect to error page
+    }
+
+/*Please, be aware that it is not safe to use JavaScript to protect web pages
+JavaScript commands are executed in the browser after the page is already loaded. */ 
