02f93e8f18e0f0e8c002f5b85ec7d0885422100a
Falcon <guiedsouza@gmail.com>
2020-04-20 12:09:15 -0300

    atualização do comentário sobre palavras no plural

--- garrafasMuro/garrafasNoMuro.js
+++ garrafasMuro/garrafasNoMuro.js
@@ -10,7 +10,7 @@ var contador = Number(prompt("QUAL A CONTAGEM INICIAL?\nmax: 5000")) //Insira o 
 
     while (contador > 0) {
         if (contador != 1) {
-        document.write(contador + " " + palavra + "s" + " de cerveja no muro.<br>") //Texto no plural e no singular com if
+        document.write(contador + " " + palavra + "s" + " de cerveja no muro.<br>") //Texto no plural e no singular com if, nem sempre vai funcionar, mas para este exercício, vai funcionar
         }
 
         if (contador != 1) {
