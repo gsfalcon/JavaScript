ced8b501fe89642240c33ac903c115891e1b2a2a
FaLcoN <guiedsouza@gmail.com>
2020-07-21 12:23:41 -0300

    Update garrafasNoMuro.js

--- head-first-javascript/001garrafasMuro/garrafasNoMuro.js
+++ head-first-javascript/001garrafasMuro/garrafasNoMuro.js
@@ -5,27 +5,27 @@ if (contador >= 5000) {
 }
 if (contador <= 0) {
     //Limitadora do contador para números menores ou iguais a zero (0)
-    document.write(
+    window.document.write(
         "Insira um valor MAIOR QUE ZERO para começar a brincadeira.<br>Aperte F5 para recarregar a página!"
     );
 }
 while (contador > 0) {
     if (contador != 1) {
-        document.write(contador + " " + palavra + "s" + " de cerveja no muro.<br>"); //Texto no plural e no singular com if, nem sempre vai funcionar, mas para este exercício, vai funcionar
+        window.document.write(contador + " " + palavra + "s" + " de cerveja no muro.<br>"); //Texto no plural e no singular com if, nem sempre vai funcionar, mas para este exercício, vai funcionar
     }
     if (contador != 1) {
-        document.write(contador + " " + palavra + "s" + " no muro.<br>");
+        window.document.write(contador + " " + palavra + "s" + " no muro.<br>");
     }
     if (contador == 1) {
-        document.write(contador + " " + palavra + " de cerveja no muro.<br>");
-        document.write(contador + " " + palavra + " no muro.<br>");
+        window.document.write(contador + " " + palavra + " de cerveja no muro.<br>");
+        window.document.write(contador + " " + palavra + " no muro.<br>");
     }
-    document.write(
+    window.document.write(
         "Se uma " + palavra + " cair no chão, quantas ficarão no muro?<br><hr>"
     );
     contador = contador - 1;
     if (contador == 0) {
-        document.write(
+        window.document.write(
             "Não sobraram " + palavra + "s" + " de cerveja no muro...<br>"
         ); //Mensagem final, quando contador for ZERO
     }
