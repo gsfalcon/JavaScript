4ab27f56d66498cac053d429b72c0984a4d1eda8
FaLcoN <guiedsouza@gmail.com>
2020-08-18 06:44:08 -0300

    update

--- senhaSimplesJS/passwordRequired.js
+++ senhaSimplesJS/passwordRequired.js
@@ -1,12 +1,12 @@
 var senha = 1234 //default password
 var senhaInserida = window.prompt("Digite a senha para continuar: ") //password entered
-    if (senha != senhaInserida) //checking password
-    {
-        window.alert("SENHA INCORRETA / WRONG PASSWORD") //wrong password error message
-        top.location.href = "error.html" //redirect to error page
-    }
+if (senha != senhaInserida) //checking password
+{
+    window.alert("SENHA INCORRETA / WRONG PASSWORD") //wrong password error message
+    top.location.href = "error.html" //redirect to error page
+}
 
 /*Please, be aware that it is not ok to use JavaScript to protect web pages
-JavaScript commands are executed in the browser after the page is already loaded. */ 
+JavaScript commands are executed in the browser after the page is already loaded. */
 
 //okddd
